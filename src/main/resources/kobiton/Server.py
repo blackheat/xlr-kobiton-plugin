4ce7107697e1214fb34c6b51b05b422d0de1d883
thangvo38 <thangvo8797@gmail.com>
2018-09-24 10:38:35 +0700

    Edit request URL in Server.py

--- src/main/resources/kobiton/Server.py
+++ src/main/resources/kobiton/Server.py
@@ -3,7 +3,7 @@ import sys
 params = { 'url': configuration.url, 'proxyHost': configuration.proxyHost, 'proxyPort': configuration.proxyPort }
 
 request = HttpRequest(params, configuration.username, configuration.password)
-response = request.get('/v1/devices')
+response = request.get('/devices')
 
 # check response status code, if is different than 200 exit with error code
 if response.getStatus() != 200:
