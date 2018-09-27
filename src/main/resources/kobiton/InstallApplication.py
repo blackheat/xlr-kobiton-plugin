6fd0a7a68454dfa1c5c65ecc46243089d8a63401
Black Heat (undefined) <blackheatinc@gmail.com>
2018-09-27 16:27:21 +0700

    Temporary Commit

--- src/main/resources/kobiton/InstallApplication.py
+++ src/main/resources/kobiton/InstallApplication.py
@@ -1,22 +1,4 @@
-param = {
-    'url': 'https://kobiton-us-east.s3.amazonaws.com/users/32809/apps'
-}
-
-request = HttpRequest(param)
-
-with open('/appdemo.apk', 'rb') as finput:
-    response = request.put('appdemo-b5a62a90-c223-11e8-bc74-b71de2b2cb30.apk?AWSAccessKeyId=AKIAJ7BONOZUJZ'+
-                           'MWR4WQ&Content-Type=application%2Foctet-stream&Expires=1538075126&Signature=CXfh'+
-                           'bVS9YHDQO%2Bqk5PX4jH1yd5E%3D&x-amz-acl=private&x-amz-meta-appid=0&x-amz-meta-crea'+
-                           'tedby=32809&x-amz-meta-organizationid=294&x-amz-meta-privateaccess=false&x-amz-ta'+
-                           'gging=unsaved%3Dtrue',
-                           finput,
-                           contentType='application/octet-stream')
-
-    print response.getResponse()
-    print response.getStatus()
-    print response.getHeaders()
-    print response.errorDump()
+import json
 
 
 def getUrl():
@@ -27,4 +9,27 @@ def getUrl():
     content = {
         'filename': 'appdemo.apk'
     }
+
+    response = request.post('', json.dumps(content), contentType='application/json')
+
+    return json.loads(response.getResponse())['url']
+
+
+def uploadS3(application_url):
+    param = {
+        'url': application_url
+    }
+
+    request = HttpRequest(param)
+
+    file_input = open("/appdemo.apk", "rb")
+
+    response = request.put("", file_input, contentType='application/octet-stream')
+    print response.getResponse()
+    print response.getStatus()
+    print response.errorDump()
+
+
+app_url = getUrl()
+uploadS3(app_url)
 
